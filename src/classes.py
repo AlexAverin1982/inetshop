62f6fa824a0b7afa635a5b9796f385b024803825
Alexander Averin <alexaverin201156@gmail.com>
2025-02-11 14:06:55 +0300

    +new_product +__products + products() getter

--- src/classes.py
+++ src/classes.py
@@ -2,53 +2,59 @@ import copy
 
 
 class Product:
-    
+
     def __init__(
-        self,
-        name: str,
-        description: str = "",
-        price: float = 0.0,
-        quantity: int = 0,
+            self,
+            name: str,
+            description: str = "",
+            price: float = 0.0,
+            quantity: int = 0,
     ):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, product_data: dict):
+        return cls(name=product_data.get('name', 'noname'),
+                   description=product_data.get('description', ''),
+                   price=product_data.get('price', 0),
+                   quantity=product_data.get('quantity', 0))
+
 
 class Category:
-
     # количество категорий
     category_count: int = 0
     # количество товаров
     product_count: int = 0
 
     def __init__(
-        self,
-        name: str,
-        description: str = "",
-        products: list[Product] = None
+            self,
+            name: str,
+            description: str = "",
+            products: list[Product] = None
     ):
         Category.category_count += 1
         self.name = name
         self.description = description
         # список товаров категории
-        self.products: list[Product] = []
+        self.__products: list[Product] = []
         self.product_names: list[str] = []
         if products:
-            self.products = products
-            self.product_names = [prod.name for prod in self.products]
-            Category.product_count += len(self.products)
+            self.__products = products
+            self.product_names = [prod.name for prod in self.__products]
+            Category.product_count += len(self.__products)
 
     def __del__(self) -> None:
         Category.category_count -= 1
 
     def add_product(
-        self,
-        product_name: str,
-        product_description: str = "",
-        product_price: float = 0.0,
-        product_quantity: int = 0,
+            self,
+            product_name: str,
+            product_description: str = "",
+            product_price: float = 0.0,
+            product_quantity: int = 0,
     ) -> Product:
         product = Product(
             name=product_name,
@@ -58,13 +64,18 @@ class Category:
         )
         Category.product_count += 1
 
-        self.products.append(product)
+        self.__products.append(product)
         self.product_names.append(product.name)
         return product
 
     def delete_product(self, product_name: str) -> None:
         if product_name in self.product_names:
             ind = self.product_names.index(product_name)
-            del self.products[ind]
+            del self.__products[ind]
             del self.product_names[ind]
             Category.product_count -= 1
+
+    @property
+    def products(self):
+        return '\n'.join([f'{prod.name}, {prod.price} руб. Остаток: {prod.quantity} шт.' for prod in self.__products])
+
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from src.classes import Category
+from src.classes import Category, Product
 
 
 def load_from_file(filename: str) -> list[Category]:
@@ -17,8 +17,8 @@ def load_from_file(filename: str) -> list[Category]:
         if isinstance(data, list):
             for cat_item in data:
                 new_cat = Category(
-                    aname=cat_item.get("name", ""),
-                    adescription=cat_item.get("description", ""),
+                    name=cat_item.get("name", ""),
+                    description=cat_item.get("description", ""),
                 )
                 products = cat_item.get("products", [])
                 for prod_item in products:
@@ -33,18 +33,34 @@ def load_from_file(filename: str) -> list[Category]:
 
 
 if __name__ == "__main__":
-    par_dir = os.path.abspath(os.path.join(__file__, os.pardir))
-    par_dir = os.path.abspath(os.path.join(par_dir, os.pardir))
-    data_filename = os.path.join(par_dir, "data", "products.json")
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    data_read = load_from_file(data_filename)
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
 
-    for cat in data_read:
-        print(f"{cat.name} : {cat.description}")
-        for prod in cat.products:
-            print(f"name: {prod.name}")
-            print(f"description: {prod.description}")
-            print(f"price: {prod.price}")
-            print(f"quantity: {prod.quantity}")
-            print("-" * 30)
-    print("*" * 50)
+    print(category1.products)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    print(category1.products)
+    print(category1.product_count)
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)

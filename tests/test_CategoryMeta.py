82f78ef9c7db1a385f94e5f396714e6cbda80a24
Alexander Averin <alexaverin201156@gmail.com>
2025-02-13 12:51:12 +0300

    15_1 beta

---
+++ tests/test_CategoryMeta.py
@@ -0,0 +1,12 @@
+from src.classes import Product, Category, CategoryMeta
+
+
+def test_total_cost(three_products: list[Product]) -> None:
+    cat = Category(name="cat1", description="test cat", products=three_products)
+    cm = CategoryMeta(category=cat)
+    expected_result = 0.0
+    for prod in three_products:
+        expected_result += prod.quantity * prod.price
+
+    assert cm.get_total_cost() == expected_result
+    assert cm.get_total_cost() == expected_result
